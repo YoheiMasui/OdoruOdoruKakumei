16c55c6f1b1b3cb73bfc4ec351b7756d2abb2c71
YoheiMasui <ym.fiy-penguin@ezweb.ne.jp>
2015-11-26 14:52:11 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+class Menu extends JFrame {
+	Menu() {
+		this.setSize(480, 620);
+		this.setTitle("Odoru Odoru Kakumei");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
+
+class Main {
+	public static void main(String argv[]) {
+		new Menu();
+	}
+}
