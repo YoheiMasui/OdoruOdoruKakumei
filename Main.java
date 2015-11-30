780d51e11cafc71b72e789d67205097bcae0ef44
Nyashiki <renkonuma@gmail.com>
2015-11-30 16:38:59 +0900

    menuの作成

--- Main.java
+++ Main.java
@@ -1,10 +1,61 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.*;
+import java.util.*;
+import java.awt.event.*;
 
-class Menu extends JFrame {
-	double colorIdx = 0;
+class MENU_INDEX {
+	static final public int Single = 0;
+	static final public int Network = 1;
+	static final public int Config = 2;
+	static final public int Exit = 3;
+}
+
+class CursorObservable extends Observable {
+	int cursor = 0;
+	public int getValue() {
+		return cursor;
+	}
+	public void setValue(int d) {
+		cursor += d;
+		if (cursor < 0) cursor = 0;
+		if (cursor > 3) cursor = 3;
+		setChanged();
+		notifyObservers();
+	}
+}
+
+class SelectableObserver extends JLabel implements Observer {
+	private CursorObservable cursorObservable;
+	private int index;
+	public SelectableObserver(CursorObservable c, String name, int index) {
+		cursorObservable = c;
+		this.index = index;
+		cursorObservable.addObserver(this);
+		this.setForeground(Color.GRAY);
+		
+		this.setFont(new Font("PakTypeNaqsh", Font.PLAIN, 32));
+		this.setPreferredSize(new Dimension(300, 100));
+		this.setHorizontalAlignment(JLabel.CENTER);
+		this.setText(name);
+		this.setAlignmentX(0.15f);
+	}
+	public void update(Observable o, Object arg) {
+		int cursor = cursorObservable.getValue();
+		if (index == cursor) {
+			this.setForeground(Color.RED);
+		} else {
+			this.setForeground(Color.GRAY);
+		}
+	}
+}
+
+class Menu extends JFrame implements KeyListener {
+	int color_red, color_green;
+	int v = 1;
 	JLabel titleLogo;
-	void Start() {
+	CursorObservable cursorObservable;
+		void Start() {
 		long error = 0;
 		int fps = 60;
 		long idealSleep = (1000 << 16) / fps;
@@ -26,38 +77,90 @@ class Menu extends JFrame {
 	}
 
 	void Update() {
-		titleLogo.setText("<html><span style='font-size:36pt; color:" + vivgyorColor() + ";'><I>Odoru Odoru Kakumei</I></span></html>");
+		titleLogo.setForeground(vivgyorColor());
+		
 	}
 
-	String vivgyorColor() {
-		final String colors[] =  {
-			"#E60012", "#DE6641", "#F5B090", "#C7000B", 
-			"#F39800", "#E8AC51", "#FCD7A1", "#D28300",
-			"#FFF100", "#F2E55C", "#C7000B", "#DFD000",
-			"#009944", "#39A869", "#A5D4AD", "#00873C",
-			"#0068B7", "#4784BF", "#A3BCE2", "#005AA0",
-			"#1D2088", "#5D5099", "#A59ACA", "#181878",
-			"#920783", "#A55B9A", "#CFA7CD", "#800073", 
-		};
-		if (colorIdx > 27) {
-			colorIdx = 0;
-		}
-		colorIdx += 0.1;
-		return colors[(int)colorIdx];
+	Color vivgyorColor() {
+		if (color_red == 0) v = 1; 
+		if (color_red == 255) v = -1;
+		color_red += v;
+		color_green += v;
+	
+		return new Color(color_red, color_green, 255);
 	}
 
 	Menu() {
+		cursorObservable = new CursorObservable();
 		this.setSize(710, 620);
 		this.setTitle("Odoru Odoru Kakumei");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
+		/* ロゴ */
 		this.getContentPane().setBackground(Color.BLACK);
-		titleLogo = titleLogo = new JLabel("<html><span style='font-size:36pt; color:#000000;'><I>Odoru Odoru Kakumei</I></span></html>",JLabel.CENTER);
+		titleLogo = new JLabel("Odoru Odoru Kakumei",JLabel.CENTER);
+		titleLogo.setFont(new Font("PakTypeNaqsh", Font.PLAIN, 52));
 		this.add(titleLogo, BorderLayout.NORTH);
-		this.setVisible(true);
+		/* メニューの要素 */
+		JPanel Selectable = new JPanel();
+		Selectable.setLayout(new BoxLayout(Selectable, BoxLayout.PAGE_AXIS));
+		//Selectable.setLayout(new FlowLayout());
+		SelectableObserver menu_single = 
+			new SelectableObserver(cursorObservable, "Single", 0);
+		SelectableObserver menu_network = 
+			new SelectableObserver(cursorObservable, "Network", 1);
+		SelectableObserver menu_config = 
+			new SelectableObserver(cursorObservable, "Config", 2);
+		SelectableObserver menu_exit = 
+			new SelectableObserver(cursorObservable, "Exit", 3);
+		Selectable.add(menu_single);
+		Selectable.add(menu_network);
+		Selectable.add(menu_config);
+		Selectable.add(menu_exit);
+		cursorObservable.setValue(0);
+		Selectable.setOpaque(false);
+		this.add(Selectable, BorderLayout.CENTER);
 
+		this.setVisible(true);
+		this.addKeyListener(this);
+		/* フルスクリーン
+			 GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().
+			 getDefaultScreenDevice();
+			 device.setFullScreenWindow(this);
+		*/
 		Start();
 	}
+
+	public void keyPressed(KeyEvent e){
+		int keyCode = e.getKeyCode();
+		switch(keyCode) {
+		case KeyEvent.VK_UP :
+			cursorObservable.setValue(-1);
+			break;
+		case KeyEvent.VK_DOWN :
+			cursorObservable.setValue(+1);
+			break;
+		case KeyEvent.VK_ENTER :
+			int idx = cursorObservable.getValue();
+			switch (idx) {
+			case MENU_INDEX.Single :
+				break;
+			case MENU_INDEX.Network :
+				break;
+			case MENU_INDEX.Config :
+				break;
+			case MENU_INDEX.Exit :
+				System.exit(0);
+				break;
+			}
+		}
+	}
+
+	public void keyReleased(KeyEvent e){
+  }
+
+  public void keyTyped(KeyEvent e){
+  }
 }
 
 class Main {
