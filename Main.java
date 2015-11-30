8e0abadcceced7832db7559bda7d9f35ef64b87c
Nyashiki <renkonuma@gmail.com>
2015-11-30 18:28:40 +0900

    configの作成

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ class MENU_INDEX {
 	static final public int Network = 1;
 	static final public int Config = 2;
 	static final public int Exit = 3;
+
+	static final public String Font = "cinecaption";//"PakTypeNaqsh";
 }
 
 class CursorObservable extends Observable {
@@ -34,7 +36,7 @@ class SelectableObserver extends JLabel implements Observer {
 		cursorObservable.addObserver(this);
 		this.setForeground(Color.GRAY);
 		
-		this.setFont(new Font("PakTypeNaqsh", Font.PLAIN, 32));
+		this.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 32));
 		this.setPreferredSize(new Dimension(300, 100));
 		this.setHorizontalAlignment(JLabel.CENTER);
 		this.setText(name);
@@ -50,12 +52,13 @@ class SelectableObserver extends JLabel implements Observer {
 	}
 }
 
-class Menu extends JFrame implements KeyListener {
+class Menu extends JFrame {
 	int color_red, color_green;
 	int v = 1;
 	JLabel titleLogo;
+	JPanel menuPanel, configPanel;
 	CursorObservable cursorObservable;
-		void Start() {
+	void Start() {
 		long error = 0;
 		int fps = 60;
 		long idealSleep = (1000 << 16) / fps;
@@ -91,16 +94,18 @@ class Menu extends JFrame implements KeyListener {
 	}
 
 	Menu() {
+		menuPanel = new JPanel();
+		configPanel = new JPanel();
 		cursorObservable = new CursorObservable();
 		this.setSize(710, 620);
 		this.setTitle("Odoru Odoru Kakumei");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		/* ロゴ */
-		this.getContentPane().setBackground(Color.BLACK);
+		menuPanel.setBackground(Color.BLACK);
 		titleLogo = new JLabel("Odoru Odoru Kakumei",JLabel.CENTER);
-		titleLogo.setFont(new Font("PakTypeNaqsh", Font.PLAIN, 52));
-		this.add(titleLogo, BorderLayout.NORTH);
+		titleLogo.setFont(new Font(MENU_INDEX.Font, Font.ITALIC, 52));
+		menuPanel.add(titleLogo, BorderLayout.NORTH);
 		/* メニューの要素 */
 		JPanel Selectable = new JPanel();
 		Selectable.setLayout(new BoxLayout(Selectable, BoxLayout.PAGE_AXIS));
@@ -119,10 +124,97 @@ class Menu extends JFrame implements KeyListener {
 		Selectable.add(menu_exit);
 		cursorObservable.setValue(0);
 		Selectable.setOpaque(false);
-		this.add(Selectable, BorderLayout.CENTER);
+		menuPanel.add(Selectable, BorderLayout.CENTER);
+		/* コンフィグ */
+		{
+			configPanel.setBackground(Color.BLACK);
+			JPanel volumePanel = new JPanel();
+			volumePanel.setLayout(new BoxLayout(volumePanel, BoxLayout.PAGE_AXIS));
+			configPanel.add(volumePanel, BorderLayout.NORTH);
+			JLabel volumeLabel = new JLabel("Volume");
+			volumeLabel.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 36));
+			volumeLabel.setForeground(Color.MAGENTA);
+			volumeLabel.setAlignmentX(0.0f);
+			volumePanel.add(volumeLabel);
+			volumePanel.setOpaque(false);
+			/* music volume */
+			JPanel music_volumePanel = new JPanel();
+			music_volumePanel.setLayout(new GridLayout(1, 2));
+			JLabel music_volumeLabel = new JLabel("Music");
+			music_volumeLabel.setHorizontalAlignment(JLabel.CENTER);
+			music_volumeLabel.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 28));
+			music_volumeLabel.setForeground(Color.GREEN);
+			music_volumePanel.add(music_volumeLabel);
+			JSlider music_volumeSlider = new JSlider();
+			music_volumeSlider.
+				setLabelTable(music_volumeSlider.createStandardLabels(20));
+			music_volumeSlider.setPreferredSize(new Dimension(300, 20));
+			music_volumeSlider.setPaintLabels(true);
+			music_volumeSlider.setOpaque(false);
+			music_volumeSlider.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 15));
+			music_volumeSlider.setForeground(Color.WHITE);
+			music_volumePanel.add(music_volumeSlider);
+			music_volumePanel.setOpaque(false);
+			volumePanel.add(music_volumePanel);
+			/* se volume */
+			JPanel se_volumePanel = new JPanel();
+			se_volumePanel.setLayout(new GridLayout(1, 2));
+			JLabel se_volumeLabel = new JLabel("SE");
+			se_volumeLabel.setHorizontalAlignment(JLabel.CENTER);
+			se_volumeLabel.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 28));
+			se_volumeLabel.setForeground(Color.GREEN);
+			se_volumePanel.add(se_volumeLabel);
+			JSlider se_volumeSlider = new JSlider();
+			se_volumeSlider.
+				setLabelTable(se_volumeSlider.createStandardLabels(20));
+			se_volumeSlider.setPreferredSize(new Dimension(300, 100));
+			se_volumeSlider.setPaintLabels(true);
+			se_volumeSlider.setOpaque(false);
+			se_volumeSlider.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 15));
+			se_volumeSlider.setForeground(Color.WHITE);
+			se_volumePanel.add(se_volumeSlider);
+			se_volumePanel.setOpaque(false);
+			volumePanel.add(se_volumePanel);
+			JPanel keyConfigPanel = new JPanel();
+			JPanel keyConfigDetailPanel = new JPanel();
+			keyConfigDetailPanel.setLayout(new GridLayout(2, 2));
+			JPanel[] keyConfigIndividualPanel = new JPanel[4];
+			JLabel[] keyConfigIndividualPanel_Arrow = new JLabel[4];
+			JButton[] keyConfigIndividualPanel_Key = new JButton[4];
+			final String[] arrows = { "UP", "RIGHT", "DOWN", "LEFT" };
+			for (int i = 0; i < 4; i++) {
+				keyConfigIndividualPanel[i] = new JPanel();
+				keyConfigIndividualPanel[i].setLayout(new GridLayout(1, 2));
+				keyConfigIndividualPanel[i].setOpaque(false);
+				keyConfigIndividualPanel_Arrow[i] = new JLabel();
+				keyConfigIndividualPanel_Arrow[i].setFont(
+										new Font(MENU_INDEX.Font, Font.PLAIN, 32));											 keyConfigIndividualPanel_Arrow[i].setForeground(Color.WHITE);
+				keyConfigIndividualPanel_Arrow[i].setText(arrows[i]);
+				keyConfigIndividualPanel_Key[i] = new JButton();
+			 
+				keyConfigIndividualPanel[i].add(keyConfigIndividualPanel_Arrow[i]);
+				keyConfigIndividualPanel[i].add(keyConfigIndividualPanel_Key[i]);
+				keyConfigDetailPanel.add(keyConfigIndividualPanel[i]);
+			}
+			JLabel keyConfigLabel = new JLabel("KeyConfig", JLabel.CENTER);
+			keyConfigLabel.setFont(new Font(MENU_INDEX.Font, Font.PLAIN, 36));
+			keyConfigLabel.setForeground(Color.MAGENTA);
+			keyConfigPanel.setLayout(new BorderLayout());
+			keyConfigPanel.add(keyConfigLabel, BorderLayout.NORTH);
+			keyConfigDetailPanel.setOpaque(false);
+			keyConfigPanel.add(keyConfigDetailPanel, BorderLayout.SOUTH);
+			keyConfigPanel.setOpaque(false);
+			configPanel.add(keyConfigPanel, BorderLayout.SOUTH);
+		}
+		/* 仕上げ */
 
+		menuPanel.addKeyListener(new menuKeyPressed());
+		menuPanel.setFocusable(true);
+		this.add(configPanel);
+		this.add(menuPanel);
+		menuPanel.setVisible(true);
+		configPanel.setVisible(false);
 		this.setVisible(true);
-		this.addKeyListener(this);
 		/* フルスクリーン
 			 GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().
 			 getDefaultScreenDevice();
@@ -131,36 +223,42 @@ class Menu extends JFrame implements KeyListener {
 		Start();
 	}
 
-	public void keyPressed(KeyEvent e){
-		int keyCode = e.getKeyCode();
-		switch(keyCode) {
-		case KeyEvent.VK_UP :
-			cursorObservable.setValue(-1);
-			break;
-		case KeyEvent.VK_DOWN :
-			cursorObservable.setValue(+1);
-			break;
-		case KeyEvent.VK_ENTER :
-			int idx = cursorObservable.getValue();
-			switch (idx) {
-			case MENU_INDEX.Single :
+	class menuKeyPressed implements KeyListener {
+		public void keyPressed(KeyEvent e){
+			int keyCode = e.getKeyCode();
+			switch(keyCode) {
+			case KeyEvent.VK_UP :
+				cursorObservable.setValue(-1);
 				break;
-			case MENU_INDEX.Network :
+			case KeyEvent.VK_DOWN :
+				cursorObservable.setValue(+1);
 				break;
-			case MENU_INDEX.Config :
-				break;
-			case MENU_INDEX.Exit :
-				System.exit(0);
-				break;
+			case KeyEvent.VK_ENTER :
+				int idx = cursorObservable.getValue();
+				switch (idx) {
+				case MENU_INDEX.Single :
+					break;
+				case MENU_INDEX.Network :
+					break;
+				case MENU_INDEX.Config :
+					getContentPane().removeAll();
+					add(configPanel);
+					configPanel.setVisible(true);
+					break;
+				case MENU_INDEX.Exit :
+					System.exit(0);
+					break;
+				}
 			}
+		}
+		public void keyReleased(KeyEvent e){
+		}
+
+		public void keyTyped(KeyEvent e){
 		}
 	}
 
-	public void keyReleased(KeyEvent e){
-  }
 
-  public void keyTyped(KeyEvent e){
-  }
 }
 
 class Main {
