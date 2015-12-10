42aca8f6773e2160b699a8df23472bac36ce86b1
YoheiMasui <ym.fiy-penguin@ezweb.ne.jp>
2015-12-10 16:15:02 +0900

    Update Main.java

--- scoreMaker/Main.java
+++ scoreMaker/Main.java
@@ -1,7 +1,16 @@
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.*;
+import java.awt.event.*;
+
 class ScoreMaker extends JFrame {
+	int lineNum = 0;
+	JTable table;
+	final int Add1s = 1;
+	final int Add10s = 2;
+	final int DeleteLine = 3;
+	final int Preview = 4;
+	DefaultTableModel tableModel;
 	ScoreMaker() {
 		this.setTitle("Score Maker");
 		this.setSize(400, 700);
@@ -13,23 +22,71 @@ class ScoreMaker extends JFrame {
 			{"30","","","",""},
 			{"45","","","",""}
 		};
-		String[] columnNames = {"f","left","down","up","right"};
 
+		String[] columnNames = {"f","left","down","up","right"};
+		tableModel = new DefaultTableModel(columnNames, 0);
+		table = new JTable(tableModel);
 		//JTable table = new JTable(tabledata, columnNames);
-		JTable table = new JTable(new DefaultTableModel(tabledata, columnNames) {
-				public boolean isCellEditable(int row, int columen) {
-					return false;
-				}
-			});
+		/* table = new JTable(new DefaultTableModel(tabledata, columnNames) {
+			 public boolean isCellEditable(int row, int columen) {
+			 return false;
+			 }
+			 }); */
 
 		JScrollPane sp = new JScrollPane(table);
 		sp.setPreferredSize(new Dimension(380,600));
 		JPanel p = new JPanel();
 		p.add(sp);
 		this.add(p, BorderLayout.CENTER);
+		//ボタン部分
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(1, 4));
+		JButton buttonAdd1s = new JButton("add 1s");
+		JButton buttonAdd10s = new JButton("add 10s");
+		JButton buttonDeleteLine = new JButton("delete Line");
+		JButton buttonPreview = new JButton("preview");
+		buttonPanel.add(buttonAdd1s);
+		buttonPanel.add(buttonAdd10s);
+		buttonPanel.add(buttonDeleteLine);
+		buttonPanel.add(buttonPreview);
+
+		buttonAdd1s.addActionListener(new ButtonClickListener(Add1s, table));
+		buttonAdd10s.addActionListener(new ButtonClickListener(Add10s, table));
+		buttonDeleteLine.addActionListener(new ButtonClickListener(DeleteLine, table));
+		buttonPreview.addActionListener(new ButtonClickListener(Preview, table));
+
+		this.add(buttonPanel, BorderLayout.SOUTH);
+
+	
 		this.setVisible(true);	
 	}
-	
+	class ButtonClickListener implements ActionListener {
+		int type;
+		ButtonClickListener(int type, JTable table) {
+			this.type = type;
+		}
+		public void actionPerformed(ActionEvent e) {
+			switch(type) {
+			case Add1s:  
+				for (int i = 0; i < 4; i++) {
+					String[] new_column = { Integer.toString(15 * lineNum++), "","","",""};
+					tableModel.addRow(new_column);
+				}
+				break;
+			case Add10s:
+				for (int i = 0; i < 40; i++) {
+					String[] new_column = { Integer.toString(15 * lineNum++), "","","",""};
+					tableModel.addRow(new_column);
+				}
+				break;
+			case DeleteLine:
+				break;
+			case Preview:
+				break;
+			}
+		}
+	}
 }
 
 class Main {
