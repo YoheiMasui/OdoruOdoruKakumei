b0d3d52596f2a3d54720fa4fb822fac1e0f4e031
fedora <fedora@localhost.localdomain>
2016-01-24 17:32:06 +0900

    delete unneeded files

--- DataServer.java
+++
@@ -1,9 +0,0 @@
-class DataServer{
-	private static String selectedFileName;
-	public static void setSelectedFileName(String fileName) {
-		selectedFileName = fileName;
-	}
-	public static String getSelectedFileName() {
-		return selectedFileName;
-	}
-}
